86f92514d636418db18999254bc406fcf6132300
Michal Materna <mimat@microsoft.com>
2018-08-06 12:31:18 +0200

    Naive limitation of the message size

--- src/services/batchService.ts
+++ src/services/batchService.ts
@@ -2,6 +2,8 @@ import { AddinsHub } from '../interfaces';
 import { BatchMessage, Message } from '../models';
 
 const BATCH_MESSAGE = 200;
+const MAXIMUM_MESSAGES = 100;
+const MAXIMUM_SIZE = 128 * 1024;
 
 export class BatchService {
     private addinsHub: AddinsHub;
@@ -9,9 +11,11 @@ export class BatchService {
 
     private timestamp?: number;
     private batchMessage: BatchMessage;
+    private currentSize: number;
 
     constructor() {
         this.batchMessage = new BatchMessage();
+        this.currentSize = 0;
     }
 
     public init = (addinsHub: AddinsHub, errorHandler: (message: string, ...optionalParams: any[]) => void) => {
@@ -22,7 +26,13 @@ export class BatchService {
     public queueMessage = (message: Message) => {
         const currentTimeStamp = Date.now();
 
-        if (this.batchMessage.data.length === 100) {
+        this.updateSize(message);
+        if (this.currentSize > MAXIMUM_SIZE) {
+            this.errorHandler('[BatchService]:queueMessage - Maximum Size of the payload reached');
+            return;
+        }
+
+        if (this.batchMessage.data.length === MAXIMUM_MESSAGES) {
             this.errorHandler('[BatchService]:queueMessage - Message Rate Limit Reach');
             return;
         }
@@ -39,10 +49,18 @@ export class BatchService {
     private sendBatchMessage = () => {
         this.addinsHub.sendMessage(this.batchMessage)
             .catch(e => {
-                this.errorHandler('[BatchService]:sendBatchMessage  FAIL', e);
+                this.errorHandler('[BatchService]:sendBatchMessage - FAIL', e);
             });
         this.timestamp = undefined;
         this.batchMessage = new BatchMessage();
+        this.currentSize = 0;
+    }
+
+    private updateSize(message: Message) {
+        this.currentSize += 32;
+        if (message.payload) {
+            this.currentSize += (encodeURI(message.payload).split(/%(?:u[0-9A-F]{2})?[0-9A-F]{2}|./).length - 1);
+        }
     }
 }
 
